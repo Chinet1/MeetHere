f15ef35c74905d12c31a53f573d032313546bfd5
Chinet <chinetcom@gmail.com>
2017-05-09 19:39:25 +0200

    Simple web service implemtation

--- app/src/main/java/com/chinet/meethere/UserActivity.java
+++ app/src/main/java/com/chinet/meethere/UserActivity.java
@@ -1,47 +1,54 @@
 package com.chinet.meethere;
 
 import android.content.Intent;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.Years;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class UserActivity extends AppCompatActivity {
 
-    private int userID = 2;
+    private int userID = 10;
+    private String url = "http://chinet.cba.pl/meethere.php?user=" + userID;
+    private String[] user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        DatabaseHandler db = new DatabaseHandler(this);
+        try {
+           user = new GetUser(this).execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
-        User user = db.getUser(userID);
-
-        String name = user.getName() + " " + user.getSurname();
-        Integer age = calculateYears(user.getDayOfBirthday());
-        String city = "City: " + user.getCity();
-        Integer daysInApp = calculateDays(user.getCreatedAt());
+        String name = user[1] + " " + user[2];
+        Integer age = calculateYears(user[5]);
+        String city = "City: " + user[4];
 
         String ageString = "Age: " + age;
-        String daysInAppString = "You are " + daysInApp + " days in here.";
 
-        TextView nameText = (TextView)findViewById(R.id.nameText);
+        TextView nameText = (TextView) findViewById(R.id.nameText);
         nameText.setText(name);
 
-        TextView ageText = (TextView)findViewById(R.id.ageText);
+        TextView ageText = (TextView) findViewById(R.id.ageText);
         ageText.setText(ageString);
 
-        TextView cityText = (TextView)findViewById(R.id.cityText);
+        TextView cityText = (TextView) findViewById(R.id.cityText);
         cityText.setText(city);
-
-        TextView diApp = (TextView)findViewById(R.id.diAppText);
-        diApp.setText(daysInAppString);
     }
 
     public void goFriends(View view) {
@@ -60,13 +67,52 @@ public class UserActivity extends AppCompatActivity {
         return years;
     }
 
-    private Integer calculateDays(String date) {
-        String[] parts = date.split(", ");
-        LocalDate dt = new LocalDate(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]),
-                Integer.parseInt(parts[2]));
-        LocalDate now = new LocalDate();
-        Days daysIns = Days.daysBetween(dt, now);
-        Integer days = daysIns.getDays();
-        return days;
+    private class GetUser extends AsyncTask<String, Void, String[]> {
+
+        private static final String TAG = "GetUser";
+        private UserActivity userActivity;
+
+        public GetUser(UserActivity userActivity){
+            this.userActivity = userActivity;
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            WebServiceHandler webServiceHandler = new WebServiceHandler();
+            String jsonStr = webServiceHandler.makeServiceCall(url);
+
+            Log.d(TAG, "Response form url: " + jsonStr);
+
+            String[] user = new String[8];
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonStr);
+
+                    JSONObject u = jsonObject.getJSONObject("user");
+
+                    user[0] = u.getString("ID");
+                    user[1] = u.getString("name");
+                    user[2] = u.getString("surname");
+                    user[3] = u.getString("email");
+                    user[4] = u.getString("city");
+                    user[5] = u.getString("dayOfBirthday");
+                    user[6] = u.getString("lastLocalization");
+                    user[7] = u.getString("createdAt");
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+            }
+
+            return user;
+        }
+
+        @Override
+        protected void onPostExecute(String[] user) {
+        }
     }
 }
